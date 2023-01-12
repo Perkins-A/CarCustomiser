a3750d9d2cfcdf87a8f19e4100079d1662d5d966
Alex Perkins <perkins.a@etoncollege.org.uk>
2023-01-12 20:49:37 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Perkins, Alexander (ABH) on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarCustomiser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarCustomiser
+//
+//  Created by Perkins, Alexander (ABH) on 12/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
